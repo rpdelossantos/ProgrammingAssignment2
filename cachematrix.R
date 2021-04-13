a5e55237aecad158d97a4c81fb2a1739d101e49c
rpdelossantos <82435105+rpdelossantos@users.noreply.github.com>
2021-04-13 15:07:12 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
+## I am setting the x as input in matrix
+## and set "e" which is the value solved as the null
+## I just changed all the reference from "mean" to "evaluate"
 makeCacheMatrix <- function(x = matrix()) {
-
+   e <- NULL
+  set <- function(y) {
+    x <<- y
+    e <<- NULL
+  }
+  get <- function() x
+  setevaluate <- function(evaluate) evaluate <<- evaluate
+  getevaluate <- function() evaluate
+  list(set = set, get = get,
+       setevaluate = setevaluate,
+       getevaluate = getevaluate)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## 
+## like what I did previously, I just changed "mean" to "evaluate" and "m" to "e"
+cacheevaluate <- function(x, ...) {
+        e <- x$getevaluate()
+  if(!is.null(e)) {
+    message("getting evaluate matrix")
+    return(e)
+  }
+  data <- x$get()
+  e <- evaluate(data, ...)
+  x$setevaluate(e)
+  e
 }
